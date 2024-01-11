3256f86c1ee8e84a1835ff1ecc581dd48b62e7aa
Gustave Curtil <gustave.curtil@tutanota.com>
2024-01-11 10:13:30 +0100

    ad dutch and english words

--- classes/LanguageGame.php
+++ classes/LanguageGame.php
@@ -9,9 +9,14 @@ class LanguageGame
         // :: is used for static functions
         // They can be called without an instance of that class being created
         // and are used mostly for more *static* types of data (a fixed set of translations in this case)
-        foreach (Data::words() as $frenchTranslation => $englishTranslation) {
+        foreach (Data::words() as $englishTranslation => $dutchTranslation) {
             // TODO: create instances of the Word class to be added to the words array
+            $this->words[] = new Word($englishTranslation, $dutchTranslation); //words[] ~= pushen JS
         }
+    }
+
+    public function randomWord(){
+        echo "hallo"; 
     }
 
     public function run(): void
@@ -20,10 +25,12 @@ class LanguageGame
 
         // Option A: user visits site first time (or wants a new word)
         // TODO: select a random word for the user to translate
+        echo $this->randomWord();
 
         // Option B: user has just submitted an answer
         // TODO: verify the answer (use the verify function in the word class) - you'll need to get the used word from the array first
         // TODO: generate a message for the user that can be shown
 
     }
+
 }
